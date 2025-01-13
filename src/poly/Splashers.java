717e0e57e322a5c4034d716700d2272fb938e97c
quinncahill <cahillquinn23@gmail.com>
2025-01-13 10:16:03 -0500

    this should make it so it does not splash walls or towers

--- src/poly/Splashers.java
+++ src/poly/Splashers.java
@@ -60,6 +60,37 @@ public class Splashers extends MovableUnit {
 
   }
 
+
+  private void splashWorld() throws GameActionException {
+    if (!splashEnemyPaint()) {
+    }
+    //when new lib function is made, put all code in that
+
+    //if not standing on allied paint, splash current location (self preservation)
+    if (!rc.senseMapInfo(rc.getLocation()).getPaint().isAlly()) {
+      rc.attack(rc.getLocation());
+    }
+
+    for (Direction dir : Lib.directionsCenter) {
+      if (rc.canSenseLocation(rc.getLocation().add(dir).add(dir))) {
+        MapInfo currentTile = rc.senseMapInfo(rc.getLocation().add(dir));
+        if (currentTile.isPassable() && !currentTile.getPaint().isAlly() && rc.canAttack(rc.getLocation().add(dir))) {
+          rc.attack(currentTile.getMapLocation());
+        }
+      }
+    }
+
+    for (Direction dir : Lib.directionsCenter) {
+      if (rc.canSenseLocation(rc.getLocation().add(dir))) {
+        MapInfo currentTile = rc.senseMapInfo(rc.getLocation().add(dir));
+        if (currentTile.isPassable() && !currentTile.getPaint().isAlly() && rc.canAttack(rc.getLocation().add(dir))) {
+          rc.attack(currentTile.getMapLocation());
+        }
+      }
+    }
+
+  }
+
   private boolean splashEnemyPaint() throws GameActionException {
     for (Direction dir : Lib.directionsCenter) {
       if (rc.canSenseLocation(rc.getLocation().add(dir))) {
@@ -83,37 +114,6 @@ public class Splashers extends MovableUnit {
     }
 
     return false;
-  }
-
-  private void splashWorld() throws GameActionException {
-    if (!splashEnemyPaint()) {
-    }
-    //when new lib function is made, put all code in that
-    if (!rc.senseMapInfo(rc.getLocation()).getPaint().isAlly()) {
-      rc.attack(rc.getLocation());
-    }
-
-    for (Direction dir : Lib.directionsCenter) {
-      if (rc.canSenseLocation(rc.getLocation().add(dir).add(dir))) {
-        MapInfo currentTile = rc.senseMapInfo(rc.getLocation().add(dir));
-        if (!currentTile.getPaint().isAlly() && rc.canAttack(rc.getLocation().add(dir))) {
-          rc.attack(currentTile.getMapLocation());
-        }
-      }
-    }
-
-    for (Direction dir : Lib.directionsCenter) {
-      if (rc.canSenseLocation(rc.getLocation().add(dir))) {
-        MapInfo currentTile = rc.senseMapInfo(rc.getLocation().add(dir));
-        if (!currentTile.getPaint().isAlly() && rc.canAttack(rc.getLocation().add(dir))) {
-          rc.attack(currentTile.getMapLocation());
-        }
-      }
-    }
-
-
-
-
   }
   protected void move() throws GameActionException {
     if (currentTask == SplasherTask.EXPLORE) {
