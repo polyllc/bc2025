f5b092c358dae9bbafcda7db56360b2c2a666393
quinncahill <quinny@QUINNS.COM>
2025-01-18 09:23:49 -0500

    tinkering with splasher alg and cleaning up code

--- src/poly/Splashers.java
+++ src/poly/Splashers.java
@@ -81,31 +81,15 @@ public class Splashers extends MovableUnit {
     if (!paintTowerLocations.isEmpty()) {
       int distance = rc.getLocation().distanceSquaredTo(paintTowerLocations.get(0));
       for (MapLocation tower : paintTowerLocations) {
-        if (distance < rc.getLocation().distanceSquaredTo(tower)) {
+        if (distance > rc.getLocation().distanceSquaredTo(tower)) {
           nearestPaintTower = tower;
           distance = rc.getLocation().distanceSquaredTo(tower);
         }
       }
     }
-
     else {
       nearestPaintTower = Lib.noLoc;
     }
-
-    /*
-    try {
-      for (MapLocation tower : towerLocations) {
-        if (rc.senseRobotAtLocation(tower).getType().equals(UnitType.LEVEL_ONE_PAINT_TOWER) ||
-                rc.senseRobotAtLocation(tower).getType().equals(UnitType.LEVEL_TWO_PAINT_TOWER) ||
-                rc.senseRobotAtLocation(tower).getType().equals(UnitType.LEVEL_THREE_PAINT_TOWER)) {
-          nearestPaintTower = tower;
-        }
-      }
-    }
-    catch (GameActionException e) {
-      nearestPaintTower = Lib.noLoc;
-    }
-     */
 
   }
 
@@ -116,37 +100,7 @@ public class Splashers extends MovableUnit {
 
   }
 
-  private boolean splashEnemyPaint() throws GameActionException {
-    for (Direction dir : Lib.directionsCenter) {
-      if (rc.canSenseLocation(rc.getLocation().add(dir))) {
-        MapInfo currentTile = rc.senseMapInfo(rc.getLocation().add(dir));
-        if (!currentTile.getPaint().isAlly() && rc.canAttack(currentTile.getMapLocation())) {
-          rc.attack(currentTile.getMapLocation());
-          return true;
-        }
-      }
-    }
-
-    for (Direction dir : Lib.directionsCenter) {
-      if (rc.canSenseLocation(rc.getLocation().add(dir).add(dir))) {
-        MapInfo currentTile = rc.senseMapInfo(rc.getLocation().add(dir));
-        if (!currentTile.getPaint().isAlly() && rc.canAttack(currentTile.getMapLocation())) {
-          //above if should be lib.isEnemyPaint() when made
-          rc.attack(currentTile.getMapLocation());
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
-
   private void splashWorld() throws GameActionException {
-
-    //if the splasher is standing on non-allied paint, splash that area IMMEDIATELY (self preservation)
-    if (!rc.senseMapInfo(rc.getLocation()).getPaint().isAlly() && rc.canAttack(rc.getLocation())) {
-      attack(rc.getLocation());
-    }
 
     //check the nearby map infos and see which one produces the best score
     MapInfo[] infos = rc.senseNearbyMapInfos(4);
@@ -161,41 +115,13 @@ public class Splashers extends MovableUnit {
     }
 
     //if the score isnt above 4, it isnt worth an attack, keep searching
-    if (maxScore > 4 && rc.canAttack(maxLocation)) {
+    if (maxScore > 3 && rc.canAttack(maxLocation)) {
       attack(maxLocation);
     }
-
-    //old code, delete later if above works
-    /*
-    for (Direction dir : Lib.directionsCenter) {
-      if (rc.canSenseLocation(rc.getLocation().add(dir).add(dir))) {
-        MapInfo currentTile = rc.senseMapInfo(rc.getLocation().add(dir));
-        if (currentTile.isPassable() && !currentTile.getPaint().isAlly() && rc.canAttack(currentTile.getMapLocation())) {
-          attack(currentTile.getMapLocation());
-        }
-      }
-    }
-
-    for (Direction dir : Lib.directionsCenter) {
-      if (rc.canSenseLocation(rc.getLocation().add(dir))) {
-        MapInfo currentTile = rc.senseMapInfo(rc.getLocation().add(dir));
-        if (!currentTile.isPassable() && !currentTile.getPaint().isAlly() && rc.canAttack(currentTile.getMapLocation())) {
-          attack(currentTile.getMapLocation());
-        }
-      }
-    }
-     */
 
   }
 
   private void attack(MapLocation loc) throws GameActionException {
-    /*
-    for (MapInfo info : rc.senseNearbyMapInfos(loc, 8)) {
-      if (info.getMark().isAlly()) {
-        return;
-      }
-    }
-     */
     rc.attack(loc);
   }
 
@@ -213,19 +139,22 @@ public class Splashers extends MovableUnit {
     for (MapInfo info : infos) {
       if (info.getMapLocation().isWithinDistanceSquared(center, 2) &&
               info.getPaint().isEnemy()) {
-        score += 5;
+        score += 4;
       }
-      else if (!info.isPassable()) {
+      if (!info.isPassable()) {
         score -= 1;
       }
-      else if (info.getPaint().isAlly()) {
-        score -= 2;
+      if (info.getPaint().isAlly()) {
+        score -= 1;
       }
-      else if (!info.getPaint().isEnemy() && !info.getPaint().isAlly()) {
-        score += 1;
+      if (!info.getPaint().isEnemy() && !info.getPaint().isAlly()) {
+        score += 2;
       }
-      else if (info.getMark().isAlly()) {
+      if (info.getMark().isAlly()) {
         score -= 100;
+      }
+      if (rc.getLocation() == center && !info.getPaint().isAlly()) {
+        score += 3;
       }
     }
 
